3d218ad757375731fda786c434c277949740aa61
Syed Jaffer Sunny <52747254+jaffasunny@users.noreply.github.com>
2023-02-19 01:11:41 +0500

    Start Screen completed

--- src/screens/StartScreen/StartSC.js
+++ src/screens/StartScreen/StartSC.js
@@ -1,6 +1,14 @@
 import React from 'react';
-import {StyleSheet, View, Image, Text} from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Image,
+  Text,
+  Button,
+  TouchableOpacity,
+} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import {Linking} from 'react-native';
 
 import GlobalStyles from '@/constants/GlobalStyles';
 
@@ -18,12 +26,49 @@ const StartSC = () => {
           <Image className="w-32 h-32" source={require('@/assets/logo.png')} />
         </View>
       </View>
-      <View className="flex items-center w-[90%] mt-16">
-        <Text
-          style={GlobalStyles.text}
-          className="text-black font-bold text-4xl text-center ">
-          Affordable quality rides made easy
-        </Text>
+      <View className="flex items-center mt-16">
+        <View className="w-5/6">
+          <Text
+            style={GlobalStyles.text}
+            className="text-black font-bold text-4xl text-center ">
+            Affordable quality rides made easy
+          </Text>
+          <Text
+            style={GlobalStyles.text}
+            className="text-black text-base text-center font-normal  mt-[26] mb-[100]">
+            With Rido’s Ridesharing, you can now book the best rides and split
+            the fare, and pay way less than traditional ride-hailing services.
+          </Text>
+
+          <View className="self-center mb-7">
+            <View style={{width: 150}}>
+              {/* <Button
+                title="Get started"
+                color="#312E81"
+                titleStyle={{
+                  color: 'black',
+                  fontSize: 16,
+                  textTransform: 'uppercase',
+                }}
+              /> */}
+              <TouchableOpacity
+                className="bg-indigo-900 py-2"
+                onPress={() => {}}>
+                <Text className="text-base text-white text-center">
+                  Get started
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+          <Text className="text-base text-black text-center">
+            Already have an account?
+          </Text>
+          <Text
+            className="text-center text-base text-indigo-900"
+            onPress={() => Linking.openURL('https://google.com')}>
+            Login
+          </Text>
+        </View>
       </View>
     </SafeAreaView>
   );
