d24273e5288a9cb4328ee5f59e99844f24b8134e
Syed Jaffer Sunny <52747254+jaffasunny@users.noreply.github.com>
2023-02-22 00:45:44 +0500

    Added Drawer

--- src/screens/Main/Main.js
+++ src/screens/Main/Main.js
@@ -1,11 +1,13 @@
-import {View, Text} from 'react-native';
+import {View, Text, Image} from 'react-native';
 import React from 'react';
+import MyDrawer from './../../utils/Drawer/Drawer';
+import {Button} from '@rneui/themed';
 
-const Main = () => {
+const Main = ({navigation}) => {
   return (
-    <View>
-      <Text>Main</Text>
-    </View>
+    <>
+      <MyDrawer />
+    </>
   );
 };
 
--- src/utils/Drawer/Drawer.js
+++ src/utils/Drawer/Drawer.js
@@ -1,31 +1,32 @@
 import * as React from 'react';
-import {View, Text, Button} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-
+import {View, Text, Image} from 'react-native';
 import {
   createDrawerNavigator,
   DrawerContentScrollView,
   DrawerItemList,
   DrawerItem,
 } from '@react-navigation/drawer';
+import {Avatar, Button, Icon} from '@rneui/themed';
 
-function Feed({navigation}) {
-  return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Feed Screen</Text>
-      <Button title="Open drawer" onPress={() => navigation.openDrawer()} />
-      <Button title="Toggle drawer" onPress={() => navigation.toggleDrawer()} />
-    </View>
-  );
-}
-
-// function Notifications() {
+// function Feed({navigation}) {
 //   return (
 //     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-//       <Text>Notifications Screen</Text>
+//       <Text>Feed Screen</Text>
+//       <Button title="Open drawer" onPress={() => navigation.openDrawer()} />
+//       <Button title="Toggle drawer" onPress={() => navigation.toggleDrawer()} />
 //     </View>
 //   );
 // }
+
+function Notifications({navigation}) {
+  return (
+    <View
+      className="bg-white"
+      style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Hello</Text>
+    </View>
+  );
+}
 
 function CustomDrawerContent(props) {
   return (
@@ -45,13 +46,46 @@ function CustomDrawerContent(props) {
 
 const Drawer = createDrawerNavigator();
 
-export default function MyDrawer() {
+export default function MyDrawer(props) {
   return (
     <Drawer.Navigator
-      drawerContent={props => <CustomDrawerContent {...props} />}
-    />
-    /* <Drawer.Screen name="Feed" component={Feed} /> */
-    /* <Drawer.Screen name="Notifications" component={Notifications} /> */
-    /* </Drawer.Navigator> */
+      screenOptions={{
+        drawerStyle: {
+          backgroundColor: '#fff',
+        },
+        header: () => {
+          return (
+            <View className="w-full bg-white flex flex-row justify-between items-center">
+              <Button color="#fff" onPress={() => navigation.openDrawer()}>
+                <Icon name="menu" color="#000" />
+              </Button>
+              <Image
+                className="w-30 h-30"
+                source={require('./../../assets/logo3.png')}
+              />
+              <Avatar
+                size={32}
+                rounded
+                title="Rd"
+                containerStyle={{backgroundColor: 'blue', marginRight: 5}}
+              />
+            </View>
+          );
+        },
+      }}
+      drawerContent={props => <CustomDrawerContent {...props} />}>
+      <Drawer.Screen
+        name="Notifications"
+        component={Notifications}
+        // options={{
+        //   headerTitle: props => {
+        //     <Image
+        //       style={{width: 50, height: 50}}
+        //       source={require('./../../assets/logo3.png')}
+        //     />;
+        //   },
+        // }}
+      />
+    </Drawer.Navigator>
   );
 }
